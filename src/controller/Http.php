d6f6cde8c9eafbd64a6d523835cb40226f7ba72c
Collin <collin@nb.cx>
2019-01-02 18:01:58 +0800

    fix

--- src/controller/Http.php
+++ src/controller/Http.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the NB Framework package.
  *
@@ -8,8 +7,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace nb\controller;
+
 /**
  * Http
  *
